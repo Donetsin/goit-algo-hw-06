02f3786ca4c504ad2d206ff2b5750ee62dcf87c0
Idonets <Idonets@morebis.net>
2024-10-23 15:10:08 +0300

    moder notes 01

--- task_03.py
+++ task_03.py
@@ -5,10 +5,32 @@ import networkx as nx
 G = t1.get_graf(is_weigted=True)
 
 # Алгоритм Дейкстри
-shortest_path = dict(nx.all_pairs_dijkstra_path(G, weight='weight'))
+def dijkstra(graph, start):
+    # Ініціалізація відстаней та множини невідвіданих вершин
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    unvisited = list(graph.keys())
 
+    while unvisited:
+        # Знаходження вершини з найменшою відстанню серед невідвіданих
+        current_vertex = min(unvisited, key=lambda vertex: distances[vertex])
+
+        # Якщо поточна відстань є нескінченністю, то ми завершили роботу
+        if distances[current_vertex] == float('infinity'):
+            break
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = distances[current_vertex] + weight
+
+            # Якщо нова відстань коротша, то оновлюємо найкоротший шлях
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+
+        # Видаляємо поточну вершину з множини невідвіданих
+        unvisited.remove(current_vertex)
+
+    return distances
+
+start = 'Залізничний вокзал'
 # Виведення найкоротших шляхів
-for start in G.nodes():
-    for end in G.nodes():
-        if start != end:
-            print(f"Найкоротший шлях від {start} до {end}: {shortest_path[start][end]}")
+print(dijkstra(G, start))
